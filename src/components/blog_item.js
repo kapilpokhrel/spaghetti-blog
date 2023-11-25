662d9b0d890f59e9ea0282444aff45ddac679d50
kapilpokhrel <kapilpokhrel100@gmail.com>
2023-11-25 14:31:43 +0545

    Using tailwind classes

--- src/components/blog_item.js
+++ src/components/blog_item.js
@@ -1,12 +1,23 @@
 import { londrina, pt_serif, mukta } from './fonts';
-import styles from './blog_item.module.css';
 
 export default function Blog_item({ title, date, text }) {
   return (
-    <div className={`container ${styles.blog_item}`}>
-      <h2 className={`${londrina.className} ${styles.title}`}>{title}</h2>
-      <span className={`${pt_serif.className} ${styles.pub_date}`}>{date}</span>
-      <div className={`${mukta.className} ${styles.blog_text}`}>{text}</div>
+    <div className={`container pb-[10px] flex flex-col`}>
+      <h2
+        className={`mb-0 font-bold text-[28px] min-[475px]:text-[30px] sm:text-[32px] md:text-[34px] text-[#10dbff] ${londrina.className}`}
+      >
+        {title}
+      </h2>
+      <span
+        className={`pl-[5px] pb-[10px] mb-[10px] border-b-[1px] border-white border-solid text-[14px] min-[475px]:text-[15px] sm:text-[16px] ${pt_serif.className}`}
+      >
+        {date}
+      </span>
+      <div
+        className={`text-[18px] min-[475px]:text-[20px] sm:text-[21px] md:text-[22px] ${mukta.className}`}
+      >
+        {text}
+      </div>
     </div>
   );
 }
--- src/components/header.js
+++ src/components/header.js
@@ -1,9 +1,10 @@
 import { londrina } from './fonts';
-import styles from './header.module.css';
 
 export default function Header({ children }) {
   return (
-    <div className={`container ${styles.header} ${londrina.className}`}>
+    <div
+      className={`container py-[25px] font-extrabold text-[36px] min-[475px]:text-[38px] sm:text-[40px] md:text-[42px] ${londrina.className}`}
+    >
       {children}
     </div>
   );
