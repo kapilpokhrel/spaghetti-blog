37eeba1754664cba9a7e195341bda7f9bec84452
kapilpokhrel <kapilpokhrel100@gmail.com>
2023-12-02 08:40:11 +0545

    Date formatted

--- src/components/blog_item.js
+++ src/components/blog_item.js
@@ -1,6 +1,8 @@
 import { londrina, pt_serif, barlow } from './fonts';
+import { parseISO, format } from 'date-fns';
 
 export default function Blog_item({ title, date, text, home }) {
+  const formatted_date = format(parseISO(date), 'LLLL d, yyyy');
   return (
     <div className={`group container pb-[10px] flex flex-col`}>
       <h2
@@ -12,7 +14,7 @@ export default function Blog_item({ title, date, text, home }) {
       <span
         className={`pl-[5px] pb-[10px] mb-[10px] border-b-[1px] border-white border-solid text-[12px] min-[475px]:text-[13px] sm:text-[14px] md:text-[15px] ${pt_serif.className}`}
       >
-        {date}
+        <time dateTime={date}>{formatted_date}</time>
       </span>
       <div className={`${barlow.className}`}>{text}</div>
     </div>
