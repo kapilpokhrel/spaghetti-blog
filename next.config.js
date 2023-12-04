2397e0e2108a2db6fed17217f0080d2a4da4b221
kapilpokhrel <kapilpokhrel100@gmail.com>
2023-12-04 22:56:33 +0545

    basePath in nextjs config and SITE_URL fixed for sitemap

--- next.config.js
+++ next.config.js
@@ -4,6 +4,7 @@
 const nextConfig = {
   output: 'export',
   distDir: 'out',
+  basePath: process.env.NEXT_PUBLIC_BASE_PATH || '',
   assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH || '',
   /*images: {
     unoptimized: true,
